dc141f5a2d2196f0ff0a55307f99ac556b6ac5dd
Jörg Oertel <tyskerjoerg@gmail.com>
2020-12-15 19:29:11 +0100

    first commit

---
+++ src/test/java/dk/si/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.si.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
